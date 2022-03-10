9573a49439d6a41c51b0afdc828994caf1b8076c
Joao <joao163marcos@gmail.com>
2022-03-10 16:07:22 -0300

    v1.0.2 - Ajustes no código

--- src/test/java/TesteFTP.java
+++ src/test/java/TesteFTP.java
@@ -1,21 +1,17 @@
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TesteFTP {
     public static void main(String[] args) throws IOException {
-        ClienteFTP novoClient = new ClienteFTP("ftp.testedominio.com", "root", "123");
+        ClienteFTP novoClient = new ClienteFTP("ftp.teste.com", "testeuser", "testespw", "testedir");
 
-        File arquivo = new File("meuteste.txt");
-        novoClient.uploadArq(arquivo, "C:\\Documentos");
-
-        novoClient.listarDir();
         novoClient.listarArq();
-
-        novoClient.criarDir("testedir");
-        novoClient.mudarDir("C:\\Videos");
-
-        novoClient.delArq("arquivoteste.txt");
-        novoClient.baixarArq("arquivoteste.txt");
+        FileInputStream arqUpload = new FileInputStream("\\C:\\Users\\Joao\\Desktop\\as.txt");
+        novoClient.uploadArq(new File("C:\\Users\\Joao\\Desktop\\as.txt"), "arquivo.txt");
     }
 }
